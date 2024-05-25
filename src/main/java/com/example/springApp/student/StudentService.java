8949bd19c6d733b1e1d888199b3b118bf53c5905
utsavbansal <ubansal1996@gmail.com>
2024-05-25 20:14:52 +0530

    Adding JPA Repository for Students

--- src/main/java/com/example/springApp/student/StudentService.java
+++ src/main/java/com/example/springApp/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.springApp.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,15 +11,23 @@ import java.util.List;
 
 @Service
 public class StudentService {
+    private  final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
 
     @GetMapping
     public List<Student> getStudents()
     {
+        return studentRepository.findAll();
 
-
-        return List.of(new Student
-                        (1L,"Utsav", LocalDate.of(1996, Month.JANUARY,5),"ubansal1996@gmail.com",28),
-                new Student
-                        (1L,"Vaus", LocalDate.of(1996, Month.JANUARY,5),"ubansal1@gmail.com",25));
+//        return List.of(new Student
+//                        (1L,"Utsav", LocalDate.of(1996, Month.JANUARY,5),"ubansal1996@gmail.com",28),
+//                new Student
+//                        (1L,"Vaus", LocalDate.of(1996, Month.JANUARY,5),"ubansal1@gmail.com",25));
     }
+
+
 }
