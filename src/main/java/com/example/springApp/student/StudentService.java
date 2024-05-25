9a2a8ebbf2da15d37a84d334821b9e5c29f67d85
utsavbansal <ubansal1996@gmail.com>
2024-05-25 22:19:22 +0530

    Added update Feature for student

--- src/main/java/com/example/springApp/student/StudentService.java
+++ src/main/java/com/example/springApp/student/StudentService.java
@@ -1,10 +1,12 @@
 package com.example.springApp.student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -42,5 +44,26 @@ public class StudentService {
                 throw  new IllegalStateException("Student with "+ studentId+" not present");
 
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student=studentRepository.findById(studentId).orElseThrow(
+                ()-> new IllegalStateException("Student with id "+studentId+" does not exist")
+                );
+        if(name!=null && name.length()>0 && !Objects.equals(student.getName(),name)){
+            student.setName(name);
+        }
+        if(email!=null && email.length()>0 && !Objects.equals(student.getEmail(),email))
+        {
+            Optional<Student> studentOptional=studentRepository.findStudentByEmail(email);
+            if(studentOptional.isPresent())
+            {
+                throw  new IllegalStateException("Email Taken");
+            }
+
+            student.setEmail(email);
+        }
+        //studentRepository.save(student);
     }
 }
